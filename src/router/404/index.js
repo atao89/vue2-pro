749f9ba6f03254df287941c8361fa5831e033f3d
atao89 <389326844@qq.com>
2021-11-12 17:48:23 +0800

    添加404页面

--- src/router/404/index.js
+++ src/router/404/index.js
@@ -4,21 +4,22 @@
  * @Author: 周涛
  * @Date: 2021-09-22 15:46:26
  * @LastEditors: 周涛
- * @LastEditTime: 2021-09-22 16:03:12
+ * @LastEditTime: 2021-11-12 17:30:46
  */
 export default [
-    {
-      path: '/404',
-      name: '404',
-      component: () => import(/* webpackChunkName: "404" */ '@/views/404.vue'),
-      hidden: true,
-      meta: {
-        layout: 'basic',
-        title: 'page-404',
-        label: 'ads'
-      },
-      // id: 3,
-      // pId: 30
-    //   icon: 'avatar'
-    }
-  ]
+  {
+    path: '/404',
+    name: '404',
+    component: () => import(/* webpackChunkName: "404" */ '@/views/404.vue'),
+    hidden: true,
+    meta: {
+      layout: 'basic',
+      title: 'page-404',
+      label: 'ads'
+    },
+  }, {
+    path: '*',
+    redirect: '/404',
+    hidden: true,
+  }
+]
--- src/views/404.vue
+++ src/views/404.vue
@@ -4,11 +4,12 @@
  * @Author: 周涛
  * @Date: 2021-09-22 15:44:26
  * @LastEditors: 周涛
- * @LastEditTime: 2021-09-22 16:10:33
+ * @LastEditTime: 2021-11-12 17:34:35
 -->
 <template>
   <div class="page-404">
-    <span>404 Not Found</span>
+    <div>404 Not Found</div>
+    <el-button @click="$router.go(-1)">返 回</el-button>
   </div>
 </template>
 
@@ -22,12 +23,14 @@ export default {
 .page-404 {
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size: 100px;
-  span {
+  div {
     color: #fff;
     background: #8080ff;
+    margin-bottom: 30px;
   }
 }
 </style>
